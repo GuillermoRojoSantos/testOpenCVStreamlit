30e3cf8ce45b9e562d3250906195bf36e9b72da6
TheRedDev <WtheRedDev@gmail.com>
2024-02-14 13:05:28 +0100

    Update tstStreamlit.py

--- tstStreamlit.py
+++ tstStreamlit.py
@@ -2,13 +2,22 @@ import cv2
 import streamlit as st
 
 st.title("Webcam Live Feed")
-run = st.checkbox('Run')
-FRAME_WINDOW = st.image([])
-camera = cv2.VideoCapture(0)
 
-while run:
-    _, frame = camera.read()
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    FRAME_WINDOW.image(frame)
-else:
-    st.write('Stopped')
+cap = cv2.VideoCapture(0)
+
+frame_placeholder = st.empty()
+stop_button_pressed = st.button("stop")
+while cap.isOpened() and not stop_button_pressed:
+    ret,frame = cap.read()
+    if not ret:
+        st.write("The capture has ended")
+        break
+
+    frame = cv.cvtColor(frame,cv2.COLOR_BRG2RGB)
+    frame_placeholder.image(frame,channels="RGB")
+
+if cv2.waitKey(1) & 0xFF == ord("q") or stop_button_pressed:
+    break
+
+cap.release()
+cv2.destroyAllWindows()
